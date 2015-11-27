255e476ad8f13ee79acb0dadec8be4e2deeabad6
PR1YANKPAT3L <silentcoder>
2015-11-26 20:05:10 -0500

    Added safe state check

--- Lab5/banker.h
+++ Lab5/banker.h
@@ -3,15 +3,31 @@
  *
  * Copyright (C) 2015, <GROUP MEMBERS>
  * All rights reserved.
- * 
+ *
  */
 #ifndef BANKER_H_
 #define BANKER_H_
 
 #include <stdbool.h>
 
-// Add any additional data structures or functions you want here
-// perhaps make a clean bank structure instead of several arrays...
+#define NUM_CUSTOMERS 5
+#define NUM_RESOURCES 3
+
+// Data structure to implement the banker's algorithm
+// Encode the state of the recourse-allocation system
+typedef struct {
+	// Available amount of each resource
+	int available[NUM_RESOURCES];
+
+	// Maximum demand of each customer
+	int maximum[NUM_CUSTOMERS][NUM_RESOURCES];
+
+	// Amount currently allocated to each customer
+	int allocation[NUM_CUSTOMERS][NUM_RESOURCES];
+
+	// Remaining need of each customer
+	int need[NUM_CUSTOMERS][NUM_RESOURCES];
+} BANK;
 
 // Request resources, returns true if successful
 extern bool request_res(int n_customer, int request[]);
